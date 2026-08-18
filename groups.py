e845c9268b646c8cbbc711d4dcf8cfc12ecf4300
Aayu <227323028+aayuutism@users.noreply.github.com>
2026-08-18 23:46:32 +0530

    Update groups.py

--- groups.py
+++ groups.py
@@ -1,13 +1,8 @@
-from groups import family_group, user_group, games_group
+from discord import app_commands
 
-if not self.tree.get_command("family"):
-    self.tree.add_command(family_group)
-    
-if not self.tree.get_command("user"):
-    self.tree.add_command(user_group)
-    
-if not self.tree.get_command("games"):
-    self.tree.add_command(games_group)
+family_group = app_commands.Group(name="family", description="Family-related commands")
+user_group = app_commands.Group(name="user", description="User-related commands")
+games_group = app_commands.Group(name="games", description="Play various mini-games!")
     
 async def setup(bot):
     pass
