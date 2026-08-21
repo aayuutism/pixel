e220c33c0b5c12d16353a812731e8b77b268ea26
Aayu <227323028+aayuutism@users.noreply.github.com>
2026-08-21 21:04:43 +0530

    Update groups.py

--- groups.py
+++ groups.py
@@ -1,9 +1,8 @@
 from discord import app_commands
 
-family_group = app_commands.Group(name="family", description="Family-related commands")
-user_group = app_commands.Group(name="user", description="User-related commands")
-games_group = app_commands.Group(name="games", description="Play various mini-games!")
-ai_group = app_commands.Group(name="ai", description="AI based commands")
+family_group = app_commands.Group(name="family")
+user_group = app_commands.Group(name="user")
+games_group = app_commands.Group(name="games")
+ai_group = app_commands.Group(name="ai")
 
-async def setup(bot):
-    pass
+async def setup(bot): pass
