22901ea23fb98f09d9dad0c3d216ee53d45a6b2f
Aayu <227323028+aayuutism@users.noreply.github.com>
2026-08-21 19:57:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,13 +21,6 @@ class MyBot(commands.Bot):
 
     async def setup_hook(self):
 
-        if not self.tree.get_command("family"):
-            self.tree.add_command(family_group)
-        if not self.tree.get_command("user"):
-            self.tree.add_command(user_group)
-        if not self.tree.get_command("games"):
-            self.tree.add_command(games_group)
-
         if os.path.exists("cogs"):
             await load_commands(self)
             
